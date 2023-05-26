e48e1009361ea582951b2e6093562cd825e520b9
{wangliling@kkworld.com} <wangliling@kkworld.com>
2023-05-26 14:57:13 +0800

    prettier config broken change

--- index.js
+++ index.js
@@ -4,10 +4,11 @@
  */
 module.exports = {
   printWidth: 100, // 单行长度
-  tabWidth: 2, // 缩进长度
+  tabWidth: 4, // 缩进长度
   useTabs: false, // 使用空格代替tab缩进
   semi: true, // 句末使用分号
   vueIndentScriptAndStyle: false, // 不对vue中的script及style标签缩进
+  singleAttributePerLine: false,
   singleQuote: true, // 使用单引号
   quoteProps: "as-needed", // 仅在必需时为对象的key添加引号
   bracketSpacing: true, // 在对象前后添加空格-eg: { foo: bar }
@@ -17,6 +18,6 @@ module.exports = {
   insertPragma: false, // 在已被prettier格式化的文件顶部加上标注
   requirePragma: false, // 无需顶部注释即可格式化
   proseWrap: "never",
-  htmlWhitespaceSensitivity: "strict", // 对HTML全局空白不敏感
+  htmlWhitespaceSensitivity: "ignore", // 对HTML全局空白不敏感
   endOfLine: "auto",
 };
